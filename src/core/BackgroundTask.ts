10388614ad83f6a72b0b7ee21099e30b9b1b4b99
Le Huy Hieu <hieu.le@mttjsc.com>
2021-03-31 19:06:20 +0700

    feat: Send file to JS

--- src/core/BackgroundTask.ts
+++ src/core/BackgroundTask.ts
@@ -1,8 +1,12 @@
-import {NativeModules} from 'react-native';
-
+import {NativeModules, DeviceEventEmitter} from 'react-native';
 const {ProcessModule} = NativeModules;
+
+const onCheckingFile = (event: any) => {
+  console.log(event);  
+};
 
 module.exports = async (taskData: any) => {
   // do stuff
   console.log({taskData});
+  DeviceEventEmitter.addListener('onCheckingFile', onCheckingFile);
 };
