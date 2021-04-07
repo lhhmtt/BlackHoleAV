efbc866c722895e5afabc5ea87a06fb1df22ab0c
lhhmtt <hieu.le@mttjsc.com>
2021-04-08 03:49:56 +0700

    chore: Remove unused code

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Text, SafeAreaView, NativeModules, TouchableOpacity } from 'react-native';
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
-import * as RNFS from 'react-native-fs';
 
 const { ProcessModule } = NativeModules;
 interface HomeProps {
@@ -9,14 +8,6 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = (props: HomeProps) => {
-  useEffect(() => {
-    RNFS.readDir(RNFS.DownloadDirectoryPath).then(files => {
-      console.log({ files })
-    })
-      .catch(err => {
-        console.log(err.message, err.code);
-      });
-  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
