0c360a29e90c0b39cd153d561c98e54cabcb8b58
Le Huy Hieu <hieu.le@mttjsc.com>
2021-04-20 17:18:24 +0700

    feat: Permissions Android

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,58 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './navigations/AppNavigator';
+import { PermissionsAndroid, ToastAndroid } from 'react-native';
 
 const App = () => {
+
+  useEffect(() => {
+    const onStartProcess = async (): Promise<void> => {
+      try {
+        const listPermissions = [
+          {
+            title: 'android.permission.READ_EXTERNAL_STORAGE' as const,
+            permission: PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          },
+          {
+            title: 'android.permission.WRITE_EXTERNAL_STORAGE' as const,
+            permission: PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          },
+        ];
+
+        const granted = await PermissionsAndroid.requestMultiple(
+          listPermissions.map((item) => item.permission),
+        );
+
+        if (
+          listPermissions.every(
+            (item) => granted[item.title] === PermissionsAndroid.RESULTS.GRANTED,
+          )
+        ) {
+
+        } else {
+          ToastAndroid.showWithGravityAndOffset(
+            "Permission is not granted!",
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+            25,
+            50
+          );
+        }
+      } catch (error) {
+        ToastAndroid.showWithGravityAndOffset(
+          error?.message || "Error",
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM,
+          25,
+          50
+        );
+      }
+    };
+    onStartProcess();
+  }, []);
+
   return (
     <>
       <AppNavigator />
